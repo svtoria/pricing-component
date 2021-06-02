8121bfa54b68fd3a0be93bd7b7efcf3a77ddec6a
Sara <sara-vitoria2002@hotmail.com>
2021-06-02 14:05:38 -0300

    a

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ const toggle = document.querySelector('input [type="checkbox"]')
 const pricesMonthly = document.getElementsByClassName("monthly")
 const pricesYearly = document.getElementsByClassName("yearly")
 
+ 
+
 })
