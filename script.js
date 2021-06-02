b5a1b66e2e4ea8763cabbe20c0c800a7670f78bc
Sara <sara-vitoria2002@hotmail.com>
2021-06-02 14:01:29 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',  ()=>{
+
+const toggle = document.querySelector('input [type="checkbox"]')
+const pricesMonthly = document.getElementsByClassName("monthly")
+const pricesYearly = document.getElementsByClassName("yearly")
+
+})
