af6d02facd8730e0a5f48f19c295902e7068972c
Sara Vitoria <78829806+svtoria@users.noreply.github.com>
2021-06-04 11:53:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-document.addEventListener('DOMContentLoaded',  ()=>{
+document.addEventListener("DOMContentLoaded", () => {
+  let toggleSwitch = document.querySelector('input[type="checkbox"]');
+  let thing = false;
 
-const toggle = document.querySelector('input [type="checkbox"]')
-const pricesMonthly = document.getElementsByClassName("monthly")
-const pricesYearly = document.getElementsByClassName("yearly")
+  let month = {
+    basic: "$19.99",
+    professional: "$24.99",
+    master: "$39.99",
+  };
 
- 
+  let year = {
+    basic: "$119.99",
+    professional: "$249.99",
+    master: "$399.99",
+  };
 
-})
+  toggleSwitch.addEventListener("change", (e) => {
+    thing = !thing;
+
+    if (thing) {
+      document.getElementById("basic").innerText = month.basic;
+      document.getElementById("professional").innerText = month.professional;
+      document.getElementById("master").innerText = month.master;
+    } else {
+      document.getElementById("basic").innerText = year.basic;
+      document.getElementById("professional").innerText = year.professional;
+      document.getElementById("master").innerText = year.master;
+    }
+  });
+});
